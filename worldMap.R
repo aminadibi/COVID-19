7d22750e99353d72a9bc49bb394f2415430e5491
Adibi <maadibi@ead.ubc.ca>
2020-02-28 14:28:14 -0800

    derivative colours added

--- worldMap.R
+++ worldMap.R
@@ -1,5 +1,5 @@
 library("ggplot2")
-theme_set(theme_bw())
+library("ggthemes")
 library("sf")
 
 library("rnaturalearth")
@@ -17,20 +17,28 @@ ggplot(data = world) +
   ggtitle("World map", subtitle = paste0("(", length(unique(world$NAME)), " countries)"))
 
 
-# Loading data
+# Loading and wrangling data
 library(readr)
+library(dplyr)
+
 time_series_19_covid_Confirmed <- read_csv("csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv")
 View(time_series_19_covid_Confirmed)
+raw <- time_series_19_covid_Confirmed %>% rename (name = "Country/Region", covidCount =  "2/27/20") %>% group_by(name) %>%
+      summarise_at(vars(`1/22/20`:covidCount), sum, na.rm = TRUE) %>% mutate(name = replace(name, name == "US", "United States")) %>%
+      mutate(name = replace(name, name == "UK", "United Kingdom")) %>% mutate(name = replace(name, name == "Mainland China", "China"))
 
-library(dplyr)
-sites <- time_series_19_covid_Confirmed %>% select ("Country/Region", "Lat", "Long", "2/27/20") %>% 
-          rename (name = "Country/Region", covidCount =  "2/27/20")
+iran <- raw %>% filter (name == "Iran")
 
+derivative <- function(sites) {
+  for (i in 1:dim(sites)[1]) {
+    for (j in length(sites):6){
+      sites[i, j] <- as.numeric(sites[i, j] - sites[i, j-1])
+    }  
+  }
+  return(sites)
+}  
 
-ggplot(data = world) +
-  geom_sf() +
-  geom_point(data = sites, aes(x = Long, y = Lat), size = 1, 
-             shape = 23, fill = "red") 
+sites <- derivative(raw)
 
 
 # merging datasets
@@ -39,5 +47,11 @@ covidWorld <- world %>% left_join(sites)
 
 # world map of Corona
 ggplot(data = covidWorld) +
-  geom_sf(aes(fill=covidCount)) + scale_fill_viridis_c(option = "plasma", trans = "sqrt")
+  geom_sf(aes(fill=covidCount)) + scale_fill_distiller(palette = "Spectral") +
+  theme_tufte()
 
+
+ggplot(data = world) +
+  geom_sf() +
+  geom_point(data = sites, aes(x = Long, y = Lat), size = 1, 
+             shape = 23, fill = "red") 
